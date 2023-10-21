9e0c7a921a3e9d70a09c9d0fcbae5f9b5cdfd6b6
Toikkanen Petronella <petronet@cubbli22-gold.local>
2023-10-21 12:06:02 +0300

    changes in sql commands to match updates in schema.sql

--- topics.py
+++ topics.py
@@ -3,13 +3,12 @@ import users
 from sqlalchemy.sql import text
 
 def get_list():
-    sql = text("SELECT id, topic from topics where visible = 1")
+    sql = text("SELECT id, topic FROM topics WHERE visible = 1")
     result = db.session.execute(sql)
     return result.fetchall()
 
 def indexinfo():
-    # hakua pitää muokata
-    sql = text("select topics.topic, COALESCE(count(conversations.topic_name), 0), COALESCE(count(messages.content), 0), COALESCE(NULLIF(MAX(messages.sent_at), NULL), NULL) from topics left join conversations on topics.topic=conversations.topic_name left join messages on conversations.convo=messages.convo_name where topics.visible=1 group by topics.id")
+    sql = text("SELECT T.topic, COALESCE(count(distinct(C.conversation)), 0), COALESCE(count(M.message), 0), COALESCE(NULLIF(MAX(M.sent_at), NULL), NULL) FROM topics T LEFT JOIN conversations C on T.id=C.topic_id LEFT JOIN messages M on C.id=M.convo_id where T.visible=1 group by T.id")
     result = db.session.execute(sql)
     return result.fetchall()
 
@@ -17,14 +16,25 @@ def create_topic(topic):
     user_id = users.user_id()
     if user_id == 0:
         return False
-    sql = text("INSERT INTO topics (topic, user_id, created) VALUES (:topic, :user_id, NOW())")
-    db.session.execute(sql, {"topic":topic, "user_id":user_id})
-    db.session.commit()
-    return True
+    sql = text("INSERT INTO topics (topic, user_id, created_at) VALUES (:topic, :user_id, NOW())")
+    
+    try:
+        db.session.execute(sql, {"topic":topic, "user_id":user_id})
+        db.session.commit()
+        return True
+    except:
+        return False
 
 
 def remove_topic(topic_id):
-    ## kun poistaa keskustelualueen, niin myös alueen keskusteluihin ja viesteihin visiblen arvoksi 0
     sql =  text("UPDATE topics SET visible=0 WHERE id=:id")
     db.session.execute(sql, {"id":topic_id})
+    db.session.commit()
+
+    sql =  text("UPDATE conversations SET visible=0 WHERE topic_id=:topic_id")
+    db.session.execute(sql, {"topic_id":topic_id})
+    db.session.commit()
+
+    sql =  text("UPDATE messages SET visible=0 WHERE topic_id=:topic_id")
+    db.session.execute(sql, {"topic_id":topic_id})
     db.session.commit()
