772e4592f2580d4b87e160de2de1370d8f4df24f
Toikkanen Petronella <petronet@cubbli22-gold.local>
2023-10-21 12:03:13 +0300

    update schema order and add secret_messages and changes in column names

--- schema.sql
+++ schema.sql
@@ -5,37 +5,41 @@ CREATE TABLE users (
     password TEXT
 );
 
-CREATE TABLE messages (
-    id SERIAL PRIMARY KEY,
-    content TEXT,
-    user_id INTEGER REFERENCES users,
-    sent_at TIMESTAMP, 
-    convo_name TEXT,
-    visible INTEGER DEFAULT 1
-);
-
-CREATE TABLE conversations (
-    id SERIAL PRIMARY KEY,
-    convo TEXT UNIQUE,
-    user_id INTEGER REFERENCES users,
-    created_at TIMESTAMP,
-    topic_name TEXT,
-    visible INTEGER DEFAULT 1
-);
-
 CREATE TABLE topics (
     id SERIAL PRIMARY KEY,
     topic TEXT UNIQUE,
     user_id INTEGER REFERENCES users,
-    created TIMESTAMP,
+    created_at TIMESTAMP,
+    visible INTEGER DEFAULT 1
+);
+
+
+CREATE TABLE conversations (
+    id SERIAL PRIMARY KEY,
+    conversation TEXT UNIQUE,
+    user_id INTEGER REFERENCES users,
+    created_at TIMESTAMP,
+    topic_id INTEGER REFERENCES topics,
+    visible INTEGER DEFAULT 1
+);
+
+
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY,
+    message TEXT,
+    user_id INTEGER REFERENCES users,
+    sent_at TIMESTAMP, 
+    altered TIMESTAMP,
+    convo_id INTEGER REFERENCES conversations,
+    topic_id INTEGER REFERENCES topics,
     visible INTEGER DEFAULT 1
 );
 
 CREATE TABLE secret_topics (
     id SERIAL PRIMARY KEY,
     secret_topic TEXT UNIQUE,
-    creator_user_id INTEGER REFERENCES users,
-    created TIMESTAMP,
+    user_id INTEGER REFERENCES users,
+    created_at TIMESTAMP,
     visible INTEGER DEFAULT 1
 );
 
@@ -44,6 +48,20 @@ CREATE TABLE secret_topics_users (
     user_id INTEGER REFERENCES users,
     PRIMARY KEY (secret_topic_id, user_id)
 );
+
+CREATE TABLE secret_messages (
+    id SERIAL PRIMARY KEY,
+    secret_message TEXT,
+    user_id INTEGER REFERENCES users,
+    sent_at TIMESTAMP, 
+    altered TIMESTAMP,
+    convo_id INTEGER REFERENCES conversations,
+    topic_id INTEGER REFERENCES topics,
+    visible INTEGER DEFAULT 1
+);
+
+
+
 
  
     
