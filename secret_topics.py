799b84ed3a0dc77a7c6596fab57bf1cf544c2e11
Toikkanen Petronella <petronet@cubbli22-gold.local>
2023-10-22 14:16:24 +0300

    added features for secret topics

--- secret_topics.py
+++ secret_topics.py
@@ -3,31 +3,30 @@ import users
 from sqlalchemy.sql import text
 
 def get_list():
-    sql = text("SELECT id, secret_topic from secret_topics where visible = 1")
+    sql = text("SELECT id, secret_topic from secret_topics ")
     result = db.session.execute(sql)
     return result.fetchall()
 
 
 def get_secret_topic_id(secret_topic):
-    sql = text("SELECT id, secret_topic from secret_topics where visible = 1 and secret_topic=:secret_topic")
+    sql = text("SELECT id from secret_topics where secret_topic=:secret_topic")
     result = db.session.execute(sql, {"secret_topic":secret_topic})
-    secret_topic = result.fetchone()
-    return secret_topic[0]
+    return result.fetchone()
 
 
 def index_list(user_id):
-    sql = text("SELECT T.secret_topic from secret_topics T, secret_topics_users U where T.id=U.secret_topic_id AND U.user_id=:user_id AND T.visible = 1 group by T.secret_topic")
+    sql = text("SELECT T.secret_topic from secret_topics T, secret_topics_users U where T.id=U.secret_topic_id AND U.user_id=:user_id group by T.secret_topic")
     result = db.session.execute(sql, {"user_id":user_id})
     return result.fetchall()
 
 
 def create_secret_topic(secret_topic):
-    creator_user_id = users.user_id()
-    if creator_user_id == 0:
+    user_id = users.user_id()
+    if user_id == 0:
         return False
-    sql = text("INSERT INTO secret_topics (secret_topic, creator_user_id, created) VALUES (:secret_topic, :creator_user_id, NOW())")
+    sql = text("INSERT INTO secret_topics (secret_topic, user_id, created_at) VALUES (:secret_topic, :user_id, NOW())")
     try: 
-        db.session.execute(sql, {"secret_topic":secret_topic, "creator_user_id":creator_user_id})
+        db.session.execute(sql, {"secret_topic":secret_topic, "user_id":user_id})
         db.session.commit()
         return True
     except:
@@ -40,7 +39,12 @@ def add_secret_topic_user(secret_topic_id, user_id):
 
 
 def remove_secret_topic(secret_topic_id):
-    ## kun poistaa keskustelualueen, niin myös alueen keskusteluihin ja viesteihin visiblen arvoksi 0
-    sql =  text("UPDATE aecrettopics SET visible=0 WHERE id=:id")
-    db.session.execute(sql, {"id":topic_id})
+    sql =  text("DELETE FROM secret_messages WHERE secret_topic_id IN (:secret_topic_id)")
+    db.session.execute(sql, {"secret_topic_id":secret_topic_id})
+
+    sql =  text("DELETE FROM secret_topics_users WHERE secret_topic_id IN (:secret_topic_id)")
+    db.session.execute(sql, {"secret_topic_id":secret_topic_id})
+
+    sql =  text("DELETE FROM secret_topics WHERE id=:secret_topic_id")
+    db.session.execute(sql, {"secret_topic_id":secret_topic_id})
     db.session.commit()
